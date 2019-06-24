e91e6befb2331dd4d452f1a0651d0936128f69c5
Nikkie Mashian <nikkiemashian@gmail.com>
2019-06-23 18:40:44 -0700

    day 2 exercises

--- ruby/day2.rb
+++ ruby/day2.rb
@@ -13,7 +13,7 @@ File.open("text.txt", "w+") { |file|
 
 # What is the benefit of the code block? More readable, and easier since there's no .close required
 
-# 2. Convert hash to an array: 
+# 2. Convert hash to an array (from docs): 
 h = { "c" => 300, "a" => 100, "d" => 400, "c" => 300  }
 h.to_a   # => [["c", 300], ["a", 100], ["d", 400]]
 
@@ -23,3 +23,27 @@ h.to_a   # => [["c", 300], ["a", 100], ["d", 400]]
 h.each { |k,v| puts "#{k} => #{v}" }
 
 #4. What other data structures do arrays support? Queues
+
+# DO: 
+# 1. Print the contents of an array of sixteen numbers, four numbers at a time, using just each. 
+i = 1
+(1..16).to_a.each do |x|
+  print(x,' ')
+  puts if (i % 4) == 0
+  i += 1
+end
+
+# Now, do the same with each_slice in Enumerable
+(1..16).to_a.each_slice(4) { |r| puts r.join(' ') }
+
+# 2. Edit the Tree class so it accepts a hash. No clue?
+
+# 3. Write a simple grep that will print the lines of a file having any occurrences of a phrase anywhere in that line
+
+def grep_fn(file, phrase)
+  File.open(file) do |f|
+    f.each { |line| puts "#{f.lineno}: #{line}" if line[phrase] }
+  end
+end
+
+grep('foo.txt', 'out')
