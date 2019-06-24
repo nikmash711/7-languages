bc170d9df86cf27282c2df003ed350440339609d
Nikkie Mashian <nikkiemashian@gmail.com>
2019-06-23 18:25:53 -0700

    find exercises

--- ruby/day2.rb
+++ ruby/day2.rb
@@ -1,4 +1,5 @@
-# Find out how to access files with and without code blocks. What is the benefit of the code block?
+# FIND: 
+# 1. Access files with and without code blocks.
 
 # Without code block: 
 file = File.open("text.txt", "w+")
@@ -10,4 +11,15 @@ File.open("text.txt", "w+") { |file|
     file.puts "Hello World"
 }
 
-# With code block is more readable, and easier since there's no .close required
+# What is the benefit of the code block? More readable, and easier since there's no .close required
+
+# 2. Convert hash to an array: 
+h = { "c" => 300, "a" => 100, "d" => 400, "c" => 300  }
+h.to_a   # => [["c", 300], ["a", 100], ["d", 400]]
+
+# Convert array to hash: Not sure?
+
+# 3. Iterate through a hash: 
+h.each { |k,v| puts "#{k} => #{v}" }
+
+#4. What other data structures do arrays support? Queues
