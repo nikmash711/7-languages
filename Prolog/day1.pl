a201d5d3f5eb4eb881f96866a4243f867b3ae97d
Nikkie Mashian <nikkiemashian@gmail.com>
2019-08-07 07:46:45 -0700

    day1 prolog

---
+++ Prolog/day1.pl
@@ -0,0 +1,10 @@
+# Make a simple knowledge base. Represent some of your favorite books and authors.
+
+book(harry_potter, jk_rowling).
+book(fantastic_beasts, jk_rowling).
+book(twilight, stephanie_myers).
+book(eclipse, stephanie_myers)
+book(goosebumps, rl_stine).
+
+# Find all books in your knowledge base written by one author.
+# ?- book(What, jk_rowling).
