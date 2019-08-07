dec231e25874a70d466e411cb6c312482f5f11f9
Nikkie Mashian <nikkiemashian@gmail.com>
2019-08-07 07:51:20 -0700

    day1 prolog

--- Prolog/day1.pl
+++ Prolog/day1.pl
@@ -1,4 +1,4 @@
-# Make a simple knowledge base. Represent some of your favorite books and authors.
+# 1. Make a simple knowledge base. Represent some of your favorite books and authors.
 
 book(harry_potter, jk_rowling).
 book(fantastic_beasts, jk_rowling).
@@ -6,5 +6,17 @@ book(twilight, stephanie_myers).
 book(eclipse, stephanie_myers)
 book(goosebumps, rl_stine).
 
-# Find all books in your knowledge base written by one author.
+# 2. Find all books in your knowledge base written by one author.
 # ?- book(What, jk_rowling).
+
+# 3. Make a knowledge base representing musicians and instruments. Also represent musicians and their genre of music.
+musician_instrument(george, guitar).
+musician_instrument(nikkie, piano).
+musician_instrument(jonathan, piano)
+musician_instrument(ethan, clarinet).
+
+musician_genre(jonathan, jazz).
+musician_genre(nikkie, contemporary).
+
+# 4. Find all musicians who play the guitar.
+# ?- musician_instrument(Who, guitar)
